696262be978e0fb715a2251ce5524478622eee9a
qasimzadehikmet <hikmetqasimov08@gmail.com>
2020-05-20 15:16:12 +0400

    taxService - Interface and Implementation

---
+++ Paycompute.Service/ITaxService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paycompute.Service
+{
+    public interface ITaxService
+    {
+        decimal TaxAmount(decimal totalAmount);
+    }
+}
