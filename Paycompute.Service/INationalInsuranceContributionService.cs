c58279b28c5f53399541d46ad312222f3bcbf7c1
qasimzadehikmet <hikmetqasimov08@gmail.com>
2020-05-20 15:39:22 +0400

    NationalInsuranceContributionService

---
+++ Paycompute.Service/INationalInsuranceContributionService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paycompute.Service
+{
+    public  interface INationalInsuranceContributionService
+    {
+        decimal NIContribution(decimal totalAmount);
+    }
+}
---
+++ Paycompute.Service/NationalInsuranceContributionService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paycompute.Service
+{
+    public class NationalInsuranceContributionService: INationalInsuranceContributionService
+    {
+        private decimal NIRate;
+        private decimal NIC;
+
+        public decimal NIContribution(decimal totalAmount)
+        {
+            if (totalAmount < 719)
+            {
+                NIRate = .0m;
+                NIC = .0m;
+            }
+            else if (totalAmount >= 719 && totalAmount <= 4167)
+            {
+                NIRate = .12m;
+                NIC = ((totalAmount - 719) * NIRate);
+            }
+            else if (totalAmount >= 4167)
+            {
+                NIRate = .02m;
+                NIC = ((4167 - 719) * .12m) + ((totalAmount - 4167) * NIRate);
+            }
+            return NIC;
+        }
+    }
+}
